4941648fc62978a1c39059e5f192fff29bd2259e
ilia1 <ilia19011991@gmail.com>
2023-03-29 19:52:44 +0300

    added SoloLearn lessons up to 64th

--- src/test/java/sololearn/lesson64/Main.java
+++ src/test/java/sololearn/lesson64/Main.java
@@ -8,22 +8,33 @@ public class Main {
     public static void main(String[ ] args) {
         ArrayList<Integer> nums = new ArrayList<Integer>();
 
-
         Scanner scanner = new Scanner(System.in);
 
+        /** заполняем ArrayList*/
         while(nums.size()<5){
             int num = scanner.nextInt();
             nums.add(num);
 
         }
 
+
         System.out.println(Collections.max(nums));
         System.out.println(Collections.min(nums));
 
+        /** в обратном порядке */
         Collections.reverse(nums);
         System.out.println(nums);
 
+        /** в обратном порядке (по итогу вернули порядок) */
+        Collections.reverse(nums);
+        System.out.println(nums);
+
+        /** Расставляем элементы в рандомном порядке */
         Collections.shuffle(nums);
+        System.out.println(nums);
+
+        /** Расставляем в порядке возрастания */
+        Collections.sort(nums);
         System.out.println(nums);
 
 
